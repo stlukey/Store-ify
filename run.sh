5207efc8a27db6824a8e024e07250c066570bbc0
Luke Southam <luke@devthe,com>
2017-07-27 03:33:15 +0100

    Improved run.sh and updated submodules.
    run.sh now uses nvm and can run from anywhere.
    
    submodules added product dimentions to admin area.

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+
+cd `dirname $(readlink -f $0)`
+
 exec 3< <(store.py/run.sh 2>&1)
 sed '/quit)$/q' <&3 ; cat <&3 &
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use v5 2> /dev/null
+
 cd store.js
 npm test
+
